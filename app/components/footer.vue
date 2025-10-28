506ce0fdece3f5d2ee837a344948f514eb4ab49c
rijalbani24 <rijalbanisalam@gmail.com>
2025-10-28 16:37:49 +0700

    uodate

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -2,11 +2,17 @@
   <div class="bg-[#fafaf9]">
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-6 text-center md:text-left"> 
     <div class="flex items-center justify-center gap-4">
-      <UIcon name="i-mdi-email" class="size-6" />
-      <UIcon name="i-mdi-instagram" class="size-6" />
-      <UIcon name="i-mdi-facebook" class="size-6" />
-      <UIcon name="i-mdi-whatsapp" class="size-6" />
+      <a href="mailto:rijalbanisalam@gmail.com" target="_blank" class="hover:text-[#ffed31] transition">
+        <UIcon name="i-mdi-email" class="size-6" />
+      </a>
+      <a href="https://www.instagram.com/rizalalatas_" target="_blank" class="hover:text-[#ffed31] transition">
+        <UIcon name="i-mdi-instagram" class="size-6" />
+      </a>
+      <a href="https://github.com/rijalbani24" target="_blank" class="hover:text-[#ffed31] transition">
+        <UIcon name="i-mdi-github" class="size-6" />
+      </a>
     </div>
+
     <div class="flex items-center text-sm font-semibold justify-center">
       Copyright © 2025 Ribasa, BBR. All Rights Reserved.
     </div>
